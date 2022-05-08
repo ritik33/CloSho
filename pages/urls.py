a6c2a31812c0c48c1d82fc4e2dcc1ffa48baabe9
Ritik Pandey <ritik.py@gmail.com>
2022-05-08 23:48:27 +0530

    removing extra stuff

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'),name='home'),
-    path('about-us', TemplateView.as_view(template_name='about-us.html'),name='about-us'),
-    path('contact-us', TemplateView.as_view(template_name='contact-us.html'),name='contact-us'),
-    # path('',TemplateView.as_view(template_name='home.html')),
-]
